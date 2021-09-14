3e8438e726471bf153cf01c01e006b5f08e50c4e
divya2603 <bhartidivya720@gmail.com>
2021-09-14 10:17:25 +0530

    Update Constants.tsx

--- next-ui-assignment/client/src/Constants.tsx
+++ next-ui-assignment/client/src/Constants.tsx
@@ -1,6 +1,6 @@
 export const NO_SEARCH_RESULTS: string = "No search results found";
 export const HEADER_NAME = "Assignment";
-export const TYPE_AHEAD_TEXT = "Please type something in the search box to to search your favourite artists, songs, albums...";
+export const TYPE_AHEAD_TEXT = "Please type something in the search box to search your favourite artists, songs, albums...";
 export const ARTIST = "Artist";
 export const ALBUM = "Album";
 export const SONG = "Song";
